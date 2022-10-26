8aff58ef32f9142043542776275852b3eb74f777
Anatoly Kushnarenko <Anatoly.kushnarenko@gmail.com>
2022-10-26 16:42:56 +0300

    Создан файл bot.py и config.py

---
+++ bot.py
@@ -0,0 +1,60 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
+from aiogram.types import BotCommand
+
+from tgbot.config import load_config
+
+logger = logging.getLogger(__name__)
+
+
+def register_all_middlewares(dp, config):
+    pass
+
+
+def register_all_filters(dp):
+    pass
+
+
+def register_all_handlers(dp):
+    pass
+
+
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
+    )
+    logger.info("Starting bot")
+    config = load_config(".env")
+
+    storage = MemoryStorage()
+    bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
+    dp = Dispatcher(bot, storage=storage)
+
+    bot['config'] = config
+
+    register_all_middlewares(dp, config)
+    register_all_filters(dp)
+    register_all_handlers(dp)
+
+    await dp.bot.set_my_commands([
+        BotCommand('start', 'Запустить бота')])
+
+    # start
+    try:
+        await dp.start_polling()
+    finally:
+        await dp.storage.close()
+        await dp.storage.wait_closed()
+        await bot.session.close()
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logger.error("Bot stopped!")
